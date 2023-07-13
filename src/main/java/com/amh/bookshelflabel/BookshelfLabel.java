e299797ab691686570be7ddf7a772a2e08ad0e0b
Abdulaziz Hassan <abdu2812.h@gmail.com>
2023-07-12 22:14:12 -0400

    Initial commit

---
+++ src/main/java/com/amh/bookshelflabel/BookshelfLabel.java
@@ -0,0 +1,16 @@
+package com.amh.bookshelflabel;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookshelfLabel implements ModInitializer {
+    public static final String MOD_ID = "bookshelflabel";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/com/amh/bookshelflabel/BookshelfLabelClient.java
@@ -0,0 +1,11 @@
+package com.amh.bookshelflabel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BookshelfLabelClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
